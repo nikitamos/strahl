522d7d515b95ece99cee06dbb7d55433a72d8334
Nikita Moskaliov <nikwit10@gmail.com>
2026-04-23 21:55:03 +0300

    fix: adjust test scene

--- backend/rcpu/src/main.rs
+++ backend/rcpu/src/main.rs
@@ -5,14 +5,18 @@ use rcpu::{
   Geometry, RayTracer, SurfaceProperty,
   camera::Camera,
   light::LightEmissionDirection,
-  material::{ConcreteMaterial, bsdf::specular::Specular, medium::UniformMedium},
+  material::{
+    ConcreteMaterial,
+    bsdf::{lambertian::Lambertian, specular::Specular},
+    medium::UniformMedium,
+  },
 };
 
 fn main() {
   let back = RayTracer::new();
   let mut scene = back.create_scene();
   let solver = back.create_solver();
-  scene.add_sphere(2.0);
+  scene.add_sphere(1.0);
   let g: Arc<dyn Geometry> = back.create_sphere(0.4);
   scene.add_light(
     Arc::clone(&g),
@@ -23,24 +27,24 @@ fn main() {
     g,
     Arc::new(ConcreteMaterial {
       medium: UniformMedium { ior: 1.0 },
-      bsdf:   Specular {
-        r: glam::vec3(0.0, 0.4, 0.3),
+      bsdf:   Lambertian {
+        s: glam::vec3(0.0, 0.4, 0.3),
       },
     }),
     rcpu::TransformParts {
-      pos:      glam::vec3(0.0, 0.0, 3.0).into(),
+      pos:      glam::vec3(0.0, -0.4, -3.0).into(),
       rotation: Quat::IDENTITY,
     },
   );
   let mut cam = Camera::new(
-    (640, 480).into(),
+    (480, 480).into(),
     Vec3::Y,
     Vec3::X,
     (5.0 * Vec3::NEG_Y).into(),
     rcpu::camera::CameraType::Perspective,
   );
   solver.render(&scene, &mut cam);
-  let mut img = image::Rgb32FImage::new(640, 480);
+  let mut img = image::Rgb32FImage::new(480, 480);
   cam.write_image(&mut img);
   img.save("out.tiff").unwrap();
 }

289fd3b8bfd951d83ead8cea8197031446b6aa57
Nikita Moskaliov <nikwit10@gmail.com>
2026-04-24 20:14:10 +0300

    fix: update assert_matches usage

--- backend/rcpu/src/lib.rs
+++ backend/rcpu/src/lib.rs
@@ -1,4 +1,3 @@
-#![feature(assert_matches)]
 #![feature(nonpoison_rwlock)]
 #![feature(sync_nonpoison)]
 
@@ -34,7 +33,6 @@ mod test;
 ///
 /// Note that macro consumes the original value.
 /// ```
-/// #![feature(assert_matches)]
 /// use std::assert_matches::assert_matches;
 /// use rcpu::with;
 ///
